155f0239eeb375d0db965eaeb5e85a00a3d2d2dd
Kazusa1412 <36687391+Kazusa1412@users.noreply.github.com>
2021-10-05 10:14:58 +0800

    init project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,23 @@
 
+buildscript {
+
+    repositories {
+        maven("https://maven.aliyun.com/nexus/content/groups/public/")
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+    }
+}
+
+allprojects {
+
+    group = "com.elouyi"
+    version = "0.0.1"
+
+    repositories {
+        maven("https://maven.aliyun.com/nexus/content/groups/public/")
+        mavenCentral()
+    }
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,8 @@
+object Versions {
+
+    const val kotlin = "1.5.30"
+    const val serialization = "1.3.0-RC"
+    const val coroutines = "1.5.1"
+    const val atomifu = "0.16.2"
+    const val ktor = "1.6.2"
+}
